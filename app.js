29566768cbdef4c38d3e85813e9f1738476c6aef
Tom Menzies <tom@192.168.1.103>
2016-10-15 16:18:37 +0100

    Updated for the Heroku port issue

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ var express = require('express');
 
 var app = express();
 
+app.set('port', (process.env.port || 5000));
+
 app.get('/', function (req,res) {
 	res.send('Hello World!');
 });
 
-app.listen(3000,function() { 
+app.listen(app.get('port'),function() { 
 	console.log('Listening on port 3000');
 });
