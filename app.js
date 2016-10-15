ff364bc2b4242014dbc5cf40efc38b763ac02c29
Tom Menzies <tom@192.168.1.103>
2016-10-15 17:40:43 +0100

    Introduce push response

--- app.js
+++ app.js
@@ -1,17 +1,31 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 
 var app = express();
+
+var list = [ "this","that","the other" ];
 
 app.set('port', (process.env.PORT || 5000));
 
 app.set('view engine', 'ejs');
 
 
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
 
 app.get('/', function (req,res) {
-	res.render('pages/index', { title: "Lol."} );
+	res.render('pages/index', { 
+		title: "Lol.",
+		data: list
+	});
+});
+
+app.post('/newdata', function (req,res) {
+	console.log(req.body);
+	list.push(req.body.todo);
+	res.redirect('/');
 });
 
 app.listen(app.get('port'),function() { 
-	console.log("Runing on ", app.get('port'));
+	console.log("Runing on", app.get('port'));
 });
