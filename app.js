a9cb0568bbb00280e50b04b6e4e811494b157678
Tom Menzies <tom@192.168.1.103>
2016-10-15 16:37:39 +0100

    Fixed capitalisation of port

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ var express = require('express');
 
 var app = express();
 
-app.set('port', (process.env.port || 5000));
+app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function (req,res) {
 	res.send('Hello World!');
 });
 
 app.listen(app.get('port'),function() { 
-	console.log('Listening on port 3000');
+	console.log;
 });
